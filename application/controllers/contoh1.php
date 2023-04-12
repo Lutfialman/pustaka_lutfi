0476e1298ed489df94a37b8897ebd9c05f152838
Lutfialman <lutfialmanpaluthi99@gmail.com>
2023-04-12 23:34:15 +0700

    first commit

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class contoh1 extends CI_Controller
+{
+    public function Index()
+    {
+        echo "<h1>perkenalkan</h1>";
+        echo" nama saya lutfi alman paluthi
+        saya tingal dipamulang hobi bersepeda";
+    }
+}
---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : lutfi</li>
+ <li>Nama Tengah : alman</li>
+ <li>Nama Belakang : paluthi</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> pamulang barat</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>tangerang</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>bola</li>
+ </ul>
+</section>
